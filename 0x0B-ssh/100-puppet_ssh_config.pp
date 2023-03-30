7ae10b588c1f8d6004486c070e84fa842388c9f0
Kevin-M254 <kmburu007@gmail.com>
2023-03-30 13:40:00 +0300

    using Puppet to make changes to our configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,6 +7,6 @@ file_line { 'Turn off passwd auth':
 
 file_line { 'Declare identity file':
   ensure => 'present',
-  path   => 'etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '	IdentityFile ~/.ssh/school',
 }
