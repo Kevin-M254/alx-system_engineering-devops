c1d12d7918712ad872d898458db43767c2567211
Kevin-M254 <kmburu007@gmail.com>
2024-07-07 11:05:48 +0300

    configuring server with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,7 +14,7 @@ exec {'Hello':
   provider => shell,
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.kolle.com\/;\\n\\t}/"/etc/nginx/sites-available/default':
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.moviequotes.com\/;\\n\\t}/"/etc/nginx/sites-available/default':
   provider => shell,
 }
 
